be720af831903f1000f271cd47952101488c67b8
Adam Roesner <roesnera@kenyon.edu>
2024-11-22 17:40:19 -0500

    Add copilot, toggleterm

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  'github/copilot.vim',
+  keys = {
+    {
+      '<leader>ce',
+      ':Copilot enable<CR>',
+      desc = 'Enable Copilot'
+    },
+    {
+      '<leader>cd',
+      ':Copilot disable<CR>',
+      desc = 'Disable Copilot'
+    }
+  }
+}
