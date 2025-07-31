2b1e694013cb96facfbdc6b5088b93db881849e4
Adam Roesner <aaroesner@gmail.com>
2025-07-31 13:18:24 -0400

    Fix for windows.nvim

--- lua/custom/plugins/windows.lua
+++ lua/custom/plugins/windows.lua
@@ -2,13 +2,13 @@ local function cmd(command)
   return table.concat({ '<Cmd>', command, '<CR>' })
 end
 return {
-  'anuvyklack/windows.nvim',
+  'JoseConseco/windows.nvim',
   dependencies = { 'anuvyklack/middleclass', 'anuvyklack/animation.nvim' },
   config = function ()
     vim.o.winwidth = 10
     vim.o.winminwidth = 10
     vim.o.equalalways = false
-    require('Windows').setup()
+    require('windows').setup()
   end,
   keys = {
     {
