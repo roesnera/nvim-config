a9f67d14c14bb1134194992699e18231c855397f
Adam Roesner <aaroesner@gmail.com>
2025-07-31 13:18:50 -0400

    The great purge
    Removes a lot of plugins that I don't really use.

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -167,20 +81,6 @@ local unmap = vim.keymap.del
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
--- map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -277,43 +177,6 @@ local function fold_fn()
   end
 end
 
-local function toggle_or_create_function_fold()
-  local ts_utils = require 'nvim-treesitter.ts_utils'
-  local current_node = ts_utils.get_node_at_cursor()
-
-  if not current_node then
-    return
-  end
-
-  local function_node = current_node
-  while function_node do
-    if function_node:type() == 'function' then
-      break
-    end
-    local parent_node = function_node:parent()
-    if not parent_node then
-      break
-    end
-
-    function_node = parent_node
-  end
-
-  if not function_node then
-    return
-  end
-
-  local start_row, start_col, end_row, end_col = function_node:range()
-  local fold_start = vim.fn.foldclosed(start_row + 1)
-  local fold_end = vim.fn.foldclosedend(start_row + 1)
-
-  if fold_start ~= -1 and fold_end == end_row + 1 then
-    vim.cmd 'normal! zd'
-  else
-    vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
-    vim.cmd 'normal! zf%'
-  end
-end
-
 map('n', 'zfe', function()
   local current_line = vim.fn.getline '.'
   if current_line:find 'function' then
@@ -394,22 +257,9 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -423,21 +273,6 @@ require('lazy').setup({
       },
     },
   },
-
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
 
   {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
@@ -679,31 +514,6 @@ require('lazy').setup({
       },
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -1161,27 +971,10 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigteLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
-    keys = {
-      { '<c-h>',  '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>',  '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>',  '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>',  '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    },
-  },
-  {
-    'nvim-treesitter/playground',
-    event = 'BufRead',
-  },
+  -- {
+  --   'nvim-treesitter/playground',
+  --   event = 'BufRead',
+  -- },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -1217,9 +1010,9 @@ local Terminal = require('toggleterm.terminal').Terminal
 
 local terminalMap = {}
 
-local function newNormalTerm(id, name, on_close_fn)
+local function newNormalTerm(id, direction, name, close_on_exit, on_close_fn)
   return {
-    direction = 'horizontal',
+    direction = direction,
     id = id,
     display_name = name,
     on_open = function()
@@ -1231,7 +1024,7 @@ local function newNormalTerm(id, name, on_close_fn)
       map('t', '<Esc><Esc>', '<C-\\><C-n>:q<CR>', { desc = 'Kill terminal' })
     end,
     on_close = on_close_fn,
-    close_on_exit = true,
+    close_on_exit = close_on_exit,
   }
 end
 
@@ -1245,7 +1038,7 @@ local function getFirstNil()
   return ind + 1
 end
 
-local mainTerm = Terminal:new(newNormalTerm(1, 'Main Term'))
+local mainTerm = Terminal:new(newNormalTerm(1, 'horizontal', 'Main Term', true))
 terminalMap[1] = mainTerm
 local lazygitTerm = Terminal:new {
   cmd = 'lazygit',
@@ -1275,7 +1068,7 @@ end, { desc = '[T]oggle the [l]azygit terminal', silent = true, noremap = true }
 local postingTerm = Terminal:new { cmd = 'posting', direction = 'float', id = 3, display_name = 'Posting Term' }
 terminalMap[3] = postingTerm
 
-local floatingTerm = Terminal:new { direction = 'float', id = 4, display_name = 'Floating Term' }
+local floatingTerm = Terminal:new(newNormalTerm(4, 'float', 'Floating Term'))
 terminalMap[4] = floatingTerm
 
 map('n', '<leader>tp', function()
@@ -1290,7 +1083,7 @@ map('n', '<leader>tn', function()
   local newTermId = getFirstNil()
   local newTermName = string.format('Regular Terminal %d', newTermId)
   print(newTermName)
-  local newTerm = newNormalTerm(newTermId, newTermName, closeAndClean)
+  local newTerm = newNormalTerm(newTermId, 'horizontal', newTermName, true, closeAndClean)
   Terminal:new(newTerm):open()
   terminalMap[newTermId] = newTerm
 end, { desc = '[T]oggle a [n]ew terminal', silent = true, noremap = true })
